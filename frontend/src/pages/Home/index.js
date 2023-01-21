35859f2d7838112a73f90895d15b82c0600ef3c9
desireevharvey <desireevharvey@gmail.com>
2023-01-20 19:07:44 -0500

    created new branch for deployment

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -3,6 +3,7 @@ import { getAllEvents } from "../../utils/api";
 import { useState, useEffect } from "react";
 import axios from "axios";
 
+
 const Home = (props) => {
     
     const [myEvents, setMyEvents] = useState([]);
@@ -10,9 +11,9 @@ const Home = (props) => {
 
     useEffect(()=>{
         getAllEvents().then((data) => {setMyEvents(data)})
-    },[props.createdEvent])
+    },[props.createdEv])
 
-   
+    console.log(myEvents)
 
    
    
@@ -24,16 +25,20 @@ const Home = (props) => {
         {myEvents.length > 0 ? 
         myEvents.map((oneEvent, i) => {
             return(
+            <div class="card">
             <div key={i}>
                 <img src={oneEvent.image} alt={oneEvent.performer}></img>
                 <p>{oneEvent.performer}</p>
                 <p>{oneEvent.venue}</p>
                 <p>{oneEvent.date}</p>
                 <p>{oneEvent.time}</p>
-                </div>)
+                </div>
+                </div>
+                )
         })
         : null}
         </>
+       
     )
 }
 export default Home
