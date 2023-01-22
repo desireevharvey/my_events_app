79d16e7925a370ae901fb1203b30434ab4181490
desireevharvey <desireevharvey@gmail.com>
2023-01-21 19:20:40 -0500

    testing2

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -23,7 +23,7 @@ return (
   <>
   <Nav isLoggedIn={isLoggedIn}/>
   <Routes>
-  <Route path="/" element={<Home createdEvent={createdEvent} />} />
+  <Route exact path="/" element={<Home createdEvent={createdEvent} />} />
   <Route path="/:id/edit" element={<EditEvent />} />
   <Route path="/event/:id" element={<ShowEvent currentUser={currentUser}/>} />
   <Route path="/login" element={<LogIn setIsLoggedIn={setIsLoggedIn} />} />
