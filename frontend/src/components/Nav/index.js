a3ee635677c11be96152b3682d118ae5d7adf4f7
desireevharvey <desireevharvey@gmail.com>
2023-02-23 15:00:39 -0500

    removed a tags

--- frontend/src/components/Nav/index.js
+++ frontend/src/components/Nav/index.js
@@ -7,55 +7,46 @@ export default function Nav(props) {
         
         <>
         {props.isLoggedIn ?
-        <nav class="navbar is-warning" role="navigation" aria-label="main navigation">
-              <div class="navbar-brand">
-    <a class="navbar-item" href="/">
+        <nav className="navbar is-warning" role="navigation" aria-label="main navigation">
+              <div className="navbar-brand">
+    <Link to='/' className="navbar-item">
       <img src="https://i.postimg.cc/3NT9t3r5/events.png" width="75" height="80">
-    </img></a>
+    </img></Link>
 
-    <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+    <a role="button" className="navbar-burger" aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
       <span aria-hidden="true"></span>
     </a>
   </div>
 
-  <div id="navbarBasicExample" class="navbar-menu">
-    <div class="navbar-start">
-    <a class="navbar-item">
-            <Link to='/'>Home</Link>
-            </a>
-            <a class="navbar-item">
-            <Link to='/newevent'>New Event</Link>
-            </a>
-            <a class="navbar-item">
-            <Link to='/account'>Account</Link>
-            </a>
+  <div id="navbarBasicExample" className="navbar-menu">
+    <div className="navbar-start">
+
+            <Link to='/' className="navbar-item">Home</Link>
+  
+            <Link to='/newevent' className="navbar-item">New Event</Link>
+          
+            <Link to='/account' className="navbar-item">Account</Link>
+      
             </div>
             </div>
         </nav>
         :
-        <nav class="navbar is-warning" role="navigation" aria-label="main navigation">
-            <div class="navbar-brand">
-                <a class="navbar-item" href="/">
-                <img src="https://i.postimg.cc/3NT9t3r5/events.png" width="75" height="80">
-                </img>
-                </a>
+        <nav className="navbar is-warning" role="navigation" aria-label="main navigation">
+            <div className="navbar-brand">
+            <Link to='/' className="navbar-item">
+      <img src="https://i.postimg.cc/3NT9t3r5/events.png" width="75" height="80">
+    </img></Link>
 
-              <div class="navbar-start">
-            <a class="navbar-item">
-            <Link to='/'>Home</Link></a>
-            <div class="navbar-end">
-      <div class="navbar-item">
-        <div class="buttons">
-          <a class="button has-background-danger-light">
-            <strong>
-            <Link to='/signup'>Sign Up</Link>
-            </strong>
-            </a>
-            <a class="button has-background-danger-light">
-            <Link to='/login'>Login</Link>
-            </a>
+
+              <div className="navbar-start">
+            <Link to='/' className="navbar-item">Home</Link>
+            <div className="navbar-end">
+      <div className="navbar-item">
+        <div className="buttons">
+            <Link to='/signup' className="button has-background-danger-light">Sign Up</Link>
+            <Link to='/login' className="button has-background-danger-light">Login</Link>  
         </div>
         </div>
       </div>
